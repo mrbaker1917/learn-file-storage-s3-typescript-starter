45f03531344525d1fd0ce7f6d9fd770f829c2d91
mrbaker1917 <mrbaker1917@gmail.com>
2026-09-23 16:28:15 -0700

    adds function to get aspect ratio and then upload by aspect

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -6,6 +6,7 @@ import { getBearerToken, validateJWT } from "../auth";
 import { getVideo, updateVideo } from "../db/videos";
 import path from "node:path";
 import { rm } from "fs/promises";
+import { json } from "node:stream/consumers";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const MAX_UPLOAD_SIZE = 1 << 30;
@@ -36,10 +37,33 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
 
   const filePath = path.join("/tmp", `${videoId}.mp4`);
   await Bun.write(filePath, file);
-  const s3file = cfg.s3Client.file(`${videoId}.mp4`, { bucket: cfg.s3Bucket});
+  const aspectRatio = await getVideoAspectRatio(filePath);
+  const s3file = cfg.s3Client.file(`${aspectRatio}/${videoId}.mp4`, { bucket: cfg.s3Bucket});
   await s3file.write(Bun.file(filePath), { type: "video/mp4"});
-  video.videoURL =  `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${videoId}.mp4`;
+  video.videoURL =  `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${aspectRatio}/${videoId}.mp4`;
   updateVideo(cfg.db, video);
   await rm(filePath, { force: true });
   return respondWithJSON(200, video);
+};
+
+export async function getVideoAspectRatio(filePath: string): Promise<string> {
+  const proc = Bun.spawn(["ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "json", filePath]);
+  const stdoutText = await new Response(proc.stdout).text();
+  const stderrText = await new Response(proc.stderr).text();
+  if (await proc.exited != 0) {
+    throw new Error(stderrText);
+  }
+  const jsonObj = JSON.parse(stdoutText);
+  const width = jsonObj["streams"][0]["width"];
+  const height = jsonObj["streams"][0]["height"];
+  const ratio = Math.floor(width/height* 100);
+  let aspectRatio = "";
+  if (ratio === Math.floor(16/9 * 100)) {
+    aspectRatio = "landscape";
+  } else if (ratio === Math.floor(9/16 * 100)) {
+    aspectRatio = "portrait";
+  } else {
+    aspectRatio = "other";
+  }
+  return aspectRatio;
 };
