46ba829654ec1c0488ba3abc563097b781d39025
mrbaker1917 <mrbaker1917@gmail.com>
2026-09-23 14:13:48 -0700

    updates for handlerUploadVideo and aws

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,10 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo, updateVideo, Video } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
+import { randomBytes } from "crypto";
+const path = require('node:path');
 type Thumbnail = {
   data: ArrayBuffer;
   mediaType: string;
@@ -32,7 +33,11 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new BadRequestError("Thumbnail file is too large.");
   };
   const mediaType = file.type;
-  const video = await getVideo(cfg.db, videoId);
+  if ((mediaType !== "image/jpeg") && (mediaType !== "image/png")) {
+    throw new BadRequestError("Thumbnail file must be a jpeg or png file.");
+  };
+  const fileExtension = mediaType.split("/")[1];
+  const video = getVideo(cfg.db, videoId);
   if (!video) {
     throw new NotFoundError("No video found with that id.");
   }
@@ -40,10 +45,13 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new UserForbiddenError("User not owner of this video");
   };
   const arrayBuffer: ArrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer).toString("base64");
-  const dataURL = `data:${mediaType};base64,${buffer}`;
+  const buffer = Buffer.from(arrayBuffer);
+  const videoUniqueAddr = randomBytes(32).toString("base64url");
+  const filePath = path.join(cfg.assetsRoot, `${videoUniqueAddr}.${fileExtension}`);
+  Bun.write(filePath, buffer);
+  const dataURL = `http://localhost:${cfg.port}/assets/${videoUniqueAddr}.${fileExtension}`;
   video.thumbnailURL =  dataURL;
-  await updateVideo(cfg.db, video);
+  updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
-}
+};
--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,8 +1,45 @@
 import { respondWithJSON } from "./json";
-
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import path from "node:path";
+import { rm } from "fs/promises";
 
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
-  return respondWithJSON(200, null);
-}
+  const MAX_UPLOAD_SIZE = 1 << 30;
+  const { videoId } = req.params as { videoId?: string };
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("No video found with that id.");
+  }
+  if (userID !== video.userID) {
+    throw new UserForbiddenError("User not owner of this video");
+  };
+  const formData = await req.formData();
+  const file = formData.get("video");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Not correct file format.");
+  };
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError("Videofile is too large.");
+  };
+  if (file.type !== "video/mp4") {
+    throw new BadRequestError("Video file not in correct format.");
+  };
+
+  const filePath = path.join("/tmp", `${videoId}.mp4`);
+  await Bun.write(filePath, file);
+  const s3file = cfg.s3Client.file(`${videoId}.mp4`, { bucket: cfg.s3Bucket});
+  await s3file.write(Bun.file(filePath), { type: "video/mp4"});
+  video.videoURL =  `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${videoId}.mp4`;
+  updateVideo(cfg.db, video);
+  await rm(filePath, { force: true });
+  return respondWithJSON(200, video);
+};
